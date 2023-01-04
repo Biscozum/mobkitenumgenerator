24bddfe42db4a32825f7dfd8d6ad78bd2a9d8664
Burakhan  Gogce <burakhang@biscozum.com.tr>
2023-01-04 14:23:08 +0300

    line length fixed

--- lib/builder.dart
+++ lib/builder.dart
@@ -4,4 +4,5 @@ import 'package:build/build.dart';
 import 'package:mobkit_enum_generator/src/enum_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder enumBuilder(BuilderOptions options) => SharedPartBuilder([EnumGenerator()], 'enum');
+Builder enumBuilder(BuilderOptions options) =>
+    SharedPartBuilder([EnumGenerator()], 'enum');
