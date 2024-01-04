fce2bf51a0939476385363e21e6af0dbf094e4ac
Burakhan Gogce <burakhang@biscozum.com.tr>
2024-01-04 13:44:04 +0300

    version upgrade completed

--- lib/builder.dart
+++ lib/builder.dart
@@ -4,6 +4,5 @@ import 'package:build/build.dart';
 import 'package:mobkit_enum_generator/src/enum_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-// It is the method that catches and triggers the EnumSerializable annotation.
-Builder enumBuilder(BuilderOptions options) =>
-    SharedPartBuilder([EnumGenerator()], 'enum');
+/// It is the method that catches and triggers the EnumSerializable annotation.
+Builder enumBuilder(BuilderOptions options) => SharedPartBuilder([EnumGenerator()], 'enum');
